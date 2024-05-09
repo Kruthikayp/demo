670f36bad41ba1db699ee3446aba63515d3fbe7c
Your Name <you@example.com>
2024-05-09 16:00:41 +0000

    basic commands

---
+++ git-command.sh
@@ -0,0 +1,15 @@
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+echo "# devops-workshop" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ManojKRISHNAPPA/devops-workshop.git
+git push -u origin main
+
+git config pull.rebase false  # mergehint:   
+git config pull.rebase true   # rebase
+git config pull.ff only       # fast-forward
+
