b46c3d089582eb675b6978340383119cfc803f06
root <root@ip-172-31-18-36.ec2.internal>
2024-05-20 07:27:40 +0000

    add new file

---
+++ linuxcommands.sh
@@ -0,0 +1,27 @@
+cd directory_path        (Change directory)
+ls                       (list directory contents)
+pwd                      (present working directory)
+mkdir directory_name     (make directory)
+rmdir directory_name      (Remove directory)
+cp [options] source destination (Copy files/directories)
+mv [options] source destination   (Move or rename files/directories)
+rm [options] file1 file2 ...      (Remove files/directories)
+touch filename                 (Create an empty file or update file timestam) 
+<B>System Information and Monitoring:</B>
+uname [options]      (Print system information)
+top                   ( Display Linux tasks)
+ps [option]  (Report a snapshot of current processes)
+df [option] ( Report file system disk space usage)
+
+du [options] [file or directory] ( Estimate file space usage)
+free [options]  ( Display amount of free and used memory in the systeme)
+
+<B>File Viewing and Editing</B>
+cat filename   (Concatenate and display files)
+more filename (view file content one screen at a time)
+less filename  (view file content with scrolling support)
+head [options] filename (output the first part of file)
+tail [options] filename (output the last part of files)
+nano filename           (commands line text edditor)
+vim filename             (improverd version of vi edditor )
+
